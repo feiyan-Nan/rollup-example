092a9e2cf30b306e0b1b0575f6a8b6d4eb5ad6fe
liguigong <liguigong@joyy.com>
2024-08-09 10:39:27 +0800

    feat: rollup的一些demo

---
+++ 1init/dist/bundle.cjs.js
@@ -0,0 +1,42 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
+})((function () { 'use strict';
+
+  function styleInject(css, ref) {
+    if ( ref === void 0 ) ref = {};
+    var insertAt = ref.insertAt;
+
+    if (typeof document === 'undefined') { return; }
+
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    style.type = 'text/css';
+
+    if (insertAt === 'top') {
+      if (head.firstChild) {
+        head.insertBefore(style, head.firstChild);
+      } else {
+        head.appendChild(style);
+      }
+    } else {
+      head.appendChild(style);
+    }
+
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+  }
+
+  var css_248z = "body{\n    background-color: gold;\n}\n";
+  styleInject(css_248z);
+
+  var myName = '张三asdfa123344444';
+  var age = 124223333333333;
+  console.log(myName, age);
+
+}));
---
+++ 3 add-react/dist/App.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+declare function App({ name }: {
+    name: string;
+}): React.JSX.Element;
+export default App;
